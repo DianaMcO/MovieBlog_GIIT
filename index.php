227f2be8f5881681c2d33df9d0d41ebcaf97e9c7
DianaMcO <dianamcode@outlook.com>
2017-06-02 13:51:49 +0100

    Add files via upload
    sorry guys, sourcetree is doing my head in.
    Here is the ammended index.php file to include the carousel.
    Also, here are the four jpegs that need to go into the images folder

--- index.php
+++ index.php
@@ -8,8 +8,47 @@ use function Movie\View\display;
 
 echo display('header');
 ?>
-<div class="container container-featured">
-     <img src="https://usercontent2.hubstatic.com/13534699.jpg" alt="Alien 2017"/> 
+<!--div class="container container-featured"-->
+    
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+        <img src="images/alien.jpg" alt="Alien">
+    </div>
+
+    <div class="item">
+        <img src="images/fastandfurious.jpg" alt="Fast and Furious">
+    </div>
+
+    <div class="item">
+        <img src="images/fantastic-four.jpg" alt="Fantastic Four">
+    </div>
+      
+      <div class="item">
+          <img src="images/spy.jpg" alt="Spy">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+     <!--img src="https://usercontent2.hubstatic.com/13534699.jpg" alt="Alien 2017"/--> 
  </div>
  
  <div class="container container-buttons">
@@ -21,7 +60,7 @@ echo display('header');
     </form>
     </div>  
     </div>
- </div>
+ <!--/div-->
 
 <div class="container container-body">
     <?php
