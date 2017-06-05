744efb2fd2a22544c38f3136955f788875db35d5
DianaMcO <dianamcode@outlook.com>
2017-06-05 08:06:38 +0100

    small modification to about page
    words 'homepage' and 'sign up' are now clickable

--- about_page.php
+++ about_page.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'common.php'; //connecting to the database
 include 'lib/Movie/Db/MovieClass.php';
 
@@ -9,25 +8,27 @@ use function Movie\View\display;
 
 echo display('header');
 ?>
-<style>
-    div {
-        text-align: center;
-    }
-</style>
 <body>
-
+    <style>
+        div {
+            text-align: center;
+        }
+    </style>
 
     <div class="container container-body" style="border-style: solid">
 
-
         <p></br></br>
         <p>We are passionate about movies and if you are too - we’d love to hear from you!</p></br>
-        <p>If you’d like to be notified of new blogs, you can subscribe on our homepage.</p></br>
-        <p>Or if you want to post comments and to enter our monthly competitions, please do sign up.</p></br>
+        <p>If you’d like to be notified of new blogs, you can subscribe on our <a href = "index.php">homepage.</a></p></br>
+        <p>Or if you want to post comments and to enter our monthly competitions, please do <a href="signup.php">sign up.</a></p></br>
         <p>Else if you just want to get in touch with us at, mail us on <a href="mailto:info@thetrainteam.com">info@thetrainteam.com</p></a></br>
     <p>And we’d love your support on social media too.</p>
 
 </div>
-</body>
 
+</footer>
+<script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
+<script src='js/main.js'></script>
+</body>
+</html>
 <?php echo display('footer'); ?>
